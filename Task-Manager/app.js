a471c2318d0520d684464b8385c9e8c9b75cb58d
girma <girmatarekegn.gi@gmail.com>
2023-06-29 21:12:29 +0300

    Add all package manager and app.js file

---
+++ Task-Manager/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const tasks = require('./routes/task')
+const connectDB = require('./db/connect');
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+require('dotenv').config()
+// middleware
+app.use(express.static('./public'));
+app.use(express.json())
+
+// routes
+
+app.use('/api/v1/tasks', tasks)
+
+app.use(errorHandlerMiddleware)
+app.use(notFound)
+
+const port = 3000;
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`server is listening on port ${port}....`));
+    } catch (error) {
+        console.log(error)
+    }
+    
+}
+
+start()
